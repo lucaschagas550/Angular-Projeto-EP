b1f36047d04b808c699632f1dd5fdf44b56eda6d
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-12-24 19:17:49 -0300

    feat: roteamento para navegar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -13,11 +18,17 @@ import { FooterComponent } from './navegacao/footer/footer.component';
     MenuComponent,
     HomeComponent,
     FooterComponent,
+    SobreComponent,
+    ContatoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false}) ]
   ],
-  providers: [],
+  providers: [{
+    provide: APP_BASE_HREF,
+    useValue: '/'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
