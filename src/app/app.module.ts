2be28f96157a08567e9ae771ff30a28db2f99e63
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-12-24 12:53:26 -0300

    feat: definindo os componentes de navegacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
